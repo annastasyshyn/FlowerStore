bf7c1fb148c36a063a239c96487e7ab2bc72a4ee
Anya Stasyshyn <anna.stasyshynn@gmail.com>
2024-10-13 19:54:12 +0300

    Change to constatnts

--- src/test/java/flower/store/FlowerBucketTest.java
+++ src/test/java/flower/store/FlowerBucketTest.java
@@ -6,14 +6,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 class FlowerBucketTest {
+    private final double delta = 0.001;
+    private final double priceRose = 10.0;
+    private final double priceTulip = 5.0;
+    private final int roseAmount = 5;
+    private final int tulipAmount = 3;
     private FlowerBucket bucket;
     private FlowerPack rosePack;
     private FlowerPack tulipPack;
-    private double delta = 0.001;
-    private double priceRose = 10.0;
-    private double priceTulip = 5.0;
-    private int roseAmount = 5;
-    private int tulipAmount = 3;
+
 
     @BeforeEach
     void setUp() {
